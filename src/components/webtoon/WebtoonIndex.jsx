f80d148dfde40dec245699e24033da41c7590050
R2DSQUAD <redface0212@naver.com>
2024-09-27 16:23:21 +0900

    오류 수정 및 웹툰 개발, 모달 개발 진행 중

--- src/components/webtoon/WebtoonIndex.jsx
+++ src/components/webtoon/WebtoonIndex.jsx
@@ -1,9 +1,134 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import WebtoonModal from "./WebtoonModal";
+
+const webtoonItems = {
+  id: 0,
+  title: "",
+  price: "",
+  img: "",
+};
+
 
 const WebtoonIndex = () => {
-  return (
-    <div>WebtoonIndex</div>
-  )
-}
+  const [isWebtoon, setIsWebtoonModal] = useState(false);
+  const [modalItem, setModalItem] = useState(webtoonItems);
+  const [webtoonItem, setWebtoonItem] = useState([]);
 
-export default WebtoonIndex
+
+  useEffect(() => {
+    const axiosFn = async () => {
+      try {
+        const res = await axios.get(`http://localhost:3001/webtoonItems`);
+        setWebtoonItem(res.data);
+      } catch (err) {
+        alert("데이터가 없습니다. 네트워크 상태를 확인해주세요.");
+      }
+    };
+    axiosFn();
+  }, []);
+
+  const webtoonModalFn = (e) => {
+    const eId = e.currentTarget.getAttribute("data-id");
+    setModalItem({
+      id: parseInt(eId)
+    });
+    setIsWebtoonModal(true);
+  };
+
+  return (
+    <>
+      {isWebtoon ? (
+        <WebtoonModal modalItem={modalItem} setIsWebtoonModal={setIsWebtoonModal}/>
+      ) : (
+        <></>
+      )}
+      <div className="webtoon">
+        <div className="webtoon-con">
+          <div className="topContent">
+            <h1>웹툰</h1>
+            <hr />
+          </div>
+          <div className="bottomContent">
+            <div className="mainBanner">
+              <ul>
+                {webtoonItem &&
+                  webtoonItem.map((el, idx) => {
+                    return (
+                      <li key={idx} data-id={el.id} onClick={webtoonModalFn}>
+                        <div className="itemImg">
+                          <img src={`/images/webtoon/${el.img}`} alt={el.img} />
+                        </div>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
+            <div className="subMenu">
+              <h2>인기웹툰</h2>
+              <ul>
+                {webtoonItem &&
+                  webtoonItem.map((el, idx) => {
+                    return (
+                      <li key={idx} data-id={el.id} onClick={webtoonModalFn}>
+                        <div className="itemImg">
+                          <img src={`/images/webtoon/${el.img}`} alt={el.img} />
+                        </div>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
+            <div className="subMenu">
+              <h2>공포 / 스릴러</h2>
+              <ul>
+                {webtoonItem &&
+                  webtoonItem.map((el, idx) => {
+                    return (
+                      <li key={idx} data-id={el.id} onClick={webtoonModalFn}>
+                        <div className="itemImg">
+                          <img src={`/images/webtoon/${el.img}`} alt={el.img} />
+                        </div>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
+            <div className="subMenu">
+              <h2>액션</h2>
+              <ul>
+                {webtoonItem &&
+                  webtoonItem.map((el, idx) => {
+                    return (
+                      <li key={idx} data-id={el.id} onClick={webtoonModalFn}>
+                        <div className="itemImg">
+                          <img src={`/images/webtoon/${el.img}`} alt={el.img} />
+                        </div>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
+            <div className="subMenu">
+              <h2>판타지 / 이세계</h2>
+              <ul>
+                {webtoonItem &&
+                  webtoonItem.map((el, idx) => {
+                    return (
+                      <li key={idx} data-id={el.id} onClick={webtoonModalFn}>
+                        <div className="itemImg">
+                          <img src={`/images/webtoon/${el.img}`} alt={el.img} />
+                        </div>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default WebtoonIndex;
--- src/router/root.jsx
+++ src/router/root.jsx
@@ -3,7 +3,8 @@ import { createBrowserRouter } from 'react-router-dom';
 
 const Loading = <div className='loading'>Loading...</div>
 const DefaultLayout = lazy(()=> import('../layout/common/DefaultLayout'));
-const MainIndex = lazy(()=> import('../components/main/MainIndex'));
+const MainIndex = lazy(()=> import('../pages/main/MainIndex'));
+const WebtoonIndex = lazy(()=> import('../components/webtoon/WebtoonIndex'));
 
 const root = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const root = createBrowserRouter([
       {
         path: '',
         element: <Suspense fallback = {Loading}><MainIndex/></Suspense>
+      },
+      {
+        path: 'webtoon',
+        element: <Suspense fallback = {Loading}><WebtoonIndex/></Suspense>
       }
     ]
   }
